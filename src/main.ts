1935606e558f3ef6ed4c764a8817acbfb0f2c52a
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-03-03 16:33:17 -0300

    add: documentação com swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('API NestJs + Prisma')
+  .setDescription('API de criação de usuário, login e criação de pedidos com produtos')
+  .setVersion('1.0.1')
+  .addTag('backend development')
+  .build();
+  
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
