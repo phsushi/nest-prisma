0e10e5f6d4a667546985c0d19486947363df8285
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-03-30 20:39:48 -0300

    new: verificando o limite de endereços por usuário e subindo exception

--- src/users/services/endereco/endereco.service.ts
+++ src/users/services/endereco/endereco.service.ts
@@ -1,4 +1,5 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, ForbiddenException, Injectable } from '@nestjs/common';
+
 import { EnderecoDto } from 'src/auth/enderecoDto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -7,18 +8,27 @@ export class EnderecoService {
     constructor(private prismaService: PrismaService){}
 
     // Auth methods //
-    createAddress(email:string, endereco: EnderecoDto){
-        //Max 2 endereos por usuário/vendedor
-        const dataEndereco = {...endereco, email}
-        return this.prismaService.endereco.create({data: dataEndereco})
-    }
-    async verifyUserExistence(email:string){
-        const userExists = await this.prismaService.usuario.findUnique({where:{email}})
+    async createAddress(email:string, endereco: EnderecoDto){
+
+        const userExists = await this.prismaService.usuario.findUnique({where:{email}});
 
         if(!userExists){
             throw new ConflictException('Usuário não existe');
         }
 
+        const usuarioId = userExists.id;
+
+        //Max 2 endereos por usuário/vendedor
+        const enderecosCadastradosDoUsuario = await this.prismaService.endereco.findMany({where:{usuarioId}})
         
+        console.log(enderecosCadastradosDoUsuario.length);
+
+        if(enderecosCadastradosDoUsuario.length > 2){
+            throw new ForbiddenException('Limite de endereços cadastrado já atingido')
+        }
+        
+        const dataEndereco = {...endereco, usuarioId}
+        return this.prismaService.endereco.create({data: dataEndereco})
     }
+   
 }
