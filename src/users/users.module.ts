b9a61ee6f27be6e529b7e39cc57d44b409763ae9
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-03-29 21:35:54 -0300

    add: injetando o service de endereo no módulo do usuario

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './controller/users/users.controller';
 import { UsersService } from './services/users/users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { EnderecoService } from './services/endereco/endereco.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService]
+  providers: [UsersService, PrismaService, EnderecoService]
 })
 export class UsersModule {}
