4dcc2bd43760e629d57fb801a5e3e014b6d9b515
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-04-08 19:05:07 -0300

    add: implementando a funcionalidade de criação de um vendededor completamente novo

--- src/users/controller/vendedor/vendedor.controller.ts
+++ src/users/controller/vendedor/vendedor.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { VendedorNovoDto } from 'src/common/Dto/vendedorNovoDto';
 import { VendedorService } from 'src/users/services/vendedor/vendedor.service';
 
 @Controller('vendedor')
 export class VendedorController {
     constructor(private vendedorService: VendedorService){}
 
-    
+    @Post('create')
+    @UsePipes(new ValidationPipe)
+    createNovoVendedor(@Body() vendedorData: VendedorNovoDto){
+        return this.vendedorService.createNovoVendedor(vendedorData);
+    }
+
 }
--- src/users/services/vendedor/vendedor.service.ts
+++ src/users/services/vendedor/vendedor.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { VendedorNovoDto } from 'src/common/Dto/vendedorNovoDto';
+import { Role } from 'src/generated/prisma/enums';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class VendedorService {
     constructor(private prismaService: PrismaService){}
 
-    
+    async createNovoVendedor(vendedorData: VendedorNovoDto){
+
+        const vendedorAlreadyExists = await this.prismaService.usuario.findUnique({where:{cpf:vendedorData.cpf}}) 
+
+        if(vendedorAlreadyExists){
+            throw new ConflictException('Vendedor já existe');
+        }
+
+        const hashedPass = await bcrypt.hash(vendedorData.senha, 10)
+
+        const {senha, ...vendedorInfo} = vendedorData
+
+        const vendedorCriado = await this.prismaService.usuario.create({
+            data:{
+                ...vendedorInfo,
+                senha: hashedPass,
+                role: Role.VENDEDOR
+            }
+        })
+
+        const {senha:_, ...result} = vendedorCriado
+        return result
+    }
 }
