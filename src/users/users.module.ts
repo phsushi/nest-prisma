20d0a7bf52cc6631c5fe98931fac086ff8cb30ed
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-04-08 17:56:15 -0300

    add: injeção do controller e service dos vendedores no módulo de user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,11 @@ import { UsersController } from './controller/users/users.controller';
 import { UsersService } from './services/users/users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { EnderecoService } from './services/endereco/endereco.service';
+import { VendedorController } from './controller/vendedor/vendedor.controller';
+import { VendedorService } from './services/vendedor/vendedor.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService, PrismaService, EnderecoService]
+  controllers: [UsersController, VendedorController],
+  providers: [UsersService, PrismaService, EnderecoService, VendedorService]
 })
 export class UsersModule {}
