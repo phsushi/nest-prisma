dc8d303826030eabb2f54718e956eb2e91647af0
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-04-22 13:25:39 -0300

    feat: subindo um usuário comum para vendedor

--- src/users/controller/vendedor/vendedor.controller.ts
+++ src/users/controller/vendedor/vendedor.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe, Request } from '@nestjs/common';
 import { VendedorNovoDto } from 'src/common/Dto/vendedorNovoDto';
+import { VendedorUpgradeUserDto } from 'src/common/Dto/vendedorUpgradeUserDto';
+import { AuthGuard } from 'src/common/guards/auth/auth.guard';
 import { VendedorService } from 'src/users/services/vendedor/vendedor.service';
 
 @Controller('vendedor')
@@ -10,6 +12,15 @@ export class VendedorController {
     @UsePipes(new ValidationPipe)
     createNovoVendedor(@Body() vendedorData: VendedorNovoDto){
         return this.vendedorService.createNovoVendedor(vendedorData);
+    }
+
+    @Post('upgrade')
+    @UseGuards(AuthGuard)
+    @UsePipes(new ValidationPipe)
+    upgradeUserToVendedor(@Body() vendedorData: VendedorUpgradeUserDto, @Request() req){
+        const email:string = req.user.email;
+
+        return this.vendedorService.upgradeUserToVendedor(email, vendedorData);
     }
 
 }
